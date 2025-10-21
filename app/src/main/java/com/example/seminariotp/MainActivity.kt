d78cfdb665f2794fa6693a3d087678b770bd9c2f
m-cappi <80648005+m-cappi@users.noreply.github.com>
2025-10-21 16:32:49 -0300

    debug hilt compatibility and error

--- app/src/main/java/com/example/seminariotp/MainActivity.kt
+++ app/src/main/java/com/example/seminariotp/MainActivity.kt
@@ -5,18 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.seminariotp.ui.games.GamesScreen
-import com.example.seminariotp.ui.theme.SeminarioTPTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlin.getValue
 
