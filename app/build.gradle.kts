1c7958f4eb9ff088d81de2a5a88b69983b607648
m-cappi <80648005+m-cappi@users.noreply.github.com>
2025-10-21 16:12:53 -0300

    add compose navigation package

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.navigation.compose)
 
     implementation(libs.hilt)
     kapt(libs.hilt.compiler)
